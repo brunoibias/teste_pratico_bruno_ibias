e8af6bab97b422e432859478ad6d9d734d6b2fcf
brunoibias <brunoibias@gmail.com>
2020-07-17 09:06:27 -0300

    teste 2/ nao substitui pelo anterior

--- tests.js
+++ tests.js
@@ -14,15 +14,27 @@ const removeRepeated = (string, limit) => {
     const defLimit = s => s.split("").reduce((a, b) => (a[a.length - limit] != b) ? (a + b) : a, "")
     return (defLimit(strings));
 }
-console.log(removeRepeated())
+console.log('A string fica assim:',removeRepeated())
 export default removeRepeated;
+
+// Teste 2
+const replaceChar = (string, times) => {
+    string = "?x?yzza?y" // definiçao do parametro string
+   times = 3 // parametro que definr a quantidade de repetiçoes a ser realizadas
+    var res = string.replace(/[?]/gi, "j".repeat(times));//substitui todos os ? quantas vezes o times indicar
+    return res;
+}
+console.log(replaceChar())
+export default replaceChar;
 
 // Teste 3
 const lettersSum = (string1, string2) => {
-    string1 = 'cgrtsr'
-    string2 = 'berGcS'
+    string1 = 'cgrtsr' // definiçao primeiro do parametro string
+    string2 = 'berGcS' // definiçao segundo do parametro string
     var strings1 = string1.toUpperCase().split("").sort();// transforma a string1 converte e um array e ordena em ordem alfabetica
     var strings2 = string2.toUpperCase().split("").sort();// transforma a string2 converte e um array e ordena em ordem alfabetica
+    console.log("A string1 ordenada ficou assim:", strings1)
+    console.log("A string2 ordenada ficou assim:", strings2)
 
     function converte(letras) {//funçao que atribui numeros as letras
         var alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -30,24 +42,24 @@ const lettersSum = (string1, string2) => {
         for (var i in letras) {
             codigos.push(alfabeto.indexOf(letras[i].toUpperCase()) + 1);
         }
+        console.log(codigos)
         return codigos;
     }
-    var resultado1 = converte(strings1)//variavel que converte a strings1 em nimeros
+    var resultado1 = converte(strings1)//variavel que converte a strings1 em numeros
     for (var i in resultado1) {
         resultado1[i]
     }
-    var resultado2 = converte(strings2)//variavel que converte a strings2 em nimeros
+    var resultado2 = converte(strings2)//variavel que converte a strings2 em numeros
     for (var i in resultado2) {
         resultado2[i]
     }
-    //retorno =(maiorLetraString1 menorLetraString2 ) + (maiorLetraString2  menorLetraString1)
+
     const ultimo2 = resultado2[resultado2.length - 1]//costante para encontrar o ultimo elemanto do array
     const ultimo1 = resultado1[resultado1.length - 1]//costante para encontrar o ultimo elemanto do array
     const som1 = (resultado1[0] * ultimo2)+(resultado2[0] * ultimo1)// executa a operaçao
     return som1;
 }
-console.log(lettersSum())
-
+console.log('O resultado de (maiorLetraString1 X menorLetraString2 ) + (maiorLetraString2 X menorLetraString1) é:',lettersSum())
 export default lettersSum;
 
 
